568b43059bcdd478836da599a6dec6ea92242dc3
paulodfigueredo <paulod.figueredo@gmail.com>
2022-11-06 00:02:01 -0300

    barra de load

--- index.js
+++ index.js
@@ -9,17 +9,19 @@ const request = {
 };
 
 function initMap() {
+  
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -13.002730, lng: -38.527716 },
     zoom: 12,
   });
 
-  request.location = new google.maps.LatLng(-13.010652, -38.486895),
+  request.location = new google.maps.LatLng(-13.010652, -38.486895)
 
   buscar()
+  move()
 }
 
-async function searchPlaces(results, status, pagination) {
+function searchPlaces(results, status, pagination) {
   if (status === google.maps.places.PlacesServiceStatus.OK) {
     let _places = []
     for (var i = 0; i < results.length; i++) {
@@ -42,7 +44,7 @@ function createPoint(item) {
     title: item.name,
     map: map,
     // icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-    icon: "https://www.iconpacks.net/icons/1/free-pin-icon-48-thumb.png",
+    // icon: "https://www.iconpacks.net/icons/1/free-pin-icon-48-thumb.png",
   });
   markers.push(marker)
 }
@@ -61,3 +63,23 @@ setTimeout(() => {
 }, 10 * 1000);
 
 window.initMap = initMap;
+
+
+function move() {
+  var elem = document.getElementById("myBar");   
+  var width = 1;
+  var id = setInterval(frame, 130);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+      document.getElementById('loadbar').remove()
+      document.getElementById('map').style.opacity = 1
+    } else if(width <= 20){
+      width = 30
+      elem.style.width = width + '%';
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+    }
+  }
+}
